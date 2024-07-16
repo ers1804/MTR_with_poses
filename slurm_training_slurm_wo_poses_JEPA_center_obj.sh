ea5754b7a341f465acd7bb17a27177dae27c1ac6
Erik Schuetz <erik.schuetz@hm.edu>
2024-07-16 08:39:34 +0200

    Adapted job descriptions and config files.

--- slurm_training_slurm_wo_poses_JEPA_center_obj.sh
+++ slurm_training_slurm_wo_poses_JEPA_center_obj.sh
@@ -35,7 +35,7 @@ cd /home/atuin/v103fe/v103fe12/MTR/tools
 
 export OMP_NUM_THREADS=64
 
-torchrun --nproc_per_node=8 --rdzv_endpoint=localhost:${PORT} train.py --launcher pytorch --cfg_file /home/atuin/v103fe/v103fe12/MTR/tools/cfgs/waymo/mtr+100_percent_data_jepa_center_obj.yaml --batch_size=96 --epochs=30 --extra_tag=Jepa_wo_poses_8a100 --tcp_port=$PORT --workers=8 --not_eval_with_train --max_ckpt_save_num=60 --set DATA_CONFIG.DATA_ROOT $TMPDIR
+torchrun --nproc_per_node=8 --rdzv_endpoint=localhost:${PORT} train.py --launcher pytorch --cfg_file /home/atuin/v103fe/v103fe12/MTR/tools/cfgs/waymo/mtr+100_percent_data_jepa_center_obj.yaml --batch_size=168 --epochs=30 --extra_tag=Jepa_wo_poses_8a100 --tcp_port=$PORT --workers=8 --not_eval_with_train --max_ckpt_save_num=60 --set DATA_CONFIG.DATA_ROOT $TMPDIR
 
 # Deactivate the virtual environment at the end
 deactivate
