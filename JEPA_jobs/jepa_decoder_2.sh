659708e58704d93fc5631b21b5882ef2f2dcfea2
Erik Schuetz <erik.schuetz@hm.edu>
2024-10-05 13:52:37 +0200

    Job descriptions and config files.

--- JEPA_jobs/jepa_decoder_2.sh
+++ JEPA_jobs/jepa_decoder_2.sh
@@ -46,7 +46,7 @@ cd /home/atuin/v103fe/v103fe12/MTR/tools
 
 export OMP_NUM_THREADS=64
 
-torchrun --nproc_per_node=8 --rdzv_endpoint=localhost:${PORT} train.py --launcher pytorch --cfg_file /home/atuin/v103fe/v103fe12/MTR/tools/cfgs/waymo/mtr+100_percent_data_jepa_with_decoder.yaml --batch_size=160 --epochs=30 --extra_tag=Jepa_decoder_no_timestamps_30 --tcp_port=$PORT --workers=8 --not_eval_with_train --max_ckpt_save_num=30 --set DATA_CONFIG.DATA_ROOT $TMPDIR MODEL.CONTEXT_ENCODER.JEPA_WEIGHTS_PATH /home/atuin/v103fe/v103fe12/no_timestamps_normal_loss_30.pth
+torchrun --nproc_per_node=8 --rdzv_endpoint=localhost:${PORT} train.py --launcher pytorch --cfg_file /home/atuin/v103fe/v103fe12/MTR/tools/cfgs/waymo/mtr+100_percent_data_jepa_with_decoder.yaml --batch_size=160 --epochs=30 --extra_tag=Jepa_decoder_no_timestamps_30 --tcp_port=$PORT --workers=8 --not_eval_with_train --max_ckpt_save_num=30 --set DATA_CONFIG.DATA_ROOT $TMPDIR MODEL.CONTEXT_ENCODER.JEPA_WEIGHTS_PATH /home/atuin/v103fe/v103fe12/no_timestamps_normal_loss_30.pth DATA_CONFIG.TIMESTAMP_EMBEDDING False MODEL.CONTEXT_ENCODER.NUM_INPUT_ATTR_AGENT 16
 
 # Deactivate the virtual environment at the end
 deactivate
--- JEPA_jobs/jepa_overfit_decoder.sh
+++ JEPA_jobs/jepa_overfit_decoder.sh
@@ -1,10 +1,10 @@
 #!/bin/bash -l
-#SBATCH --job-name=jepa_loss_trial
-#SBATCH --output=/home/atuin/v103fe/v103fe12/outputs/jepa_overfit_%j.txt
+#SBATCH --job-name=jepa_decoder
+#SBATCH --output=/home/atuin/v103fe/v103fe12/outputs/jepa_overfit_decoder_%j.txt
 #SBATCH --time=24:00:00
 #SBATCH --nodes=1
 #SBATCH --cpus-per-task=8
-#SBATCH --gres=gpu:a100:1 -C a100_40
+#SBATCH --gres=gpu:a100:1 -C a100_80
 #SBATCH --export=NONE
 
 unset SLURM_EXPORT_ENV
@@ -22,6 +22,7 @@ STORAGE_DIR="$(ws_find jepa_data)"
 # the -P parameter defines the number of parallel processes, something like 4-8 should work well
 ls -1 $STORAGE_DIR/archives_val | xargs -P 8 -I{} tar xzf $STORAGE_DIR/archives_val/{} -C $TMPDIR
 ls -1 $STORAGE_DIR/archives_train | xargs -P 8 -I{} tar xzf $STORAGE_DIR/archives_train/{} -C $TMPDIR
+
 cp $WORK/processed_scenarios_training_infos.pkl $TMPDIR/processed_scenarios_training_infos.pkl
 cp $WORK/processed_scenarios_val_infos.pkl $TMPDIR/processed_scenarios_val_infos.pkl
 
@@ -45,7 +46,7 @@ cd /home/atuin/v103fe/v103fe12/MTR/tools
 
 export OMP_NUM_THREADS=8
 
-torchrun --nproc_per_node=1 --rdzv_endpoint=localhost:${PORT} train.py --launcher pytorch --cfg_file /home/atuin/v103fe/v103fe12/MTR/tools/cfgs/waymo/jepa_loss_trial_no_timestamps_batch_norm.yaml --batch_size=20 --epochs=400 --extra_tag=Overfit_wo_t_wo_a_1_1_0 --tcp_port=$PORT --workers=8 --max_ckpt_save_num=10 --not_eval_with_train --scenario_id 103718491a34269 3c4c59a7d820467 cd7fdd75bfeddbe7 f13e51dfe26339b3 d2b602df5a0b1a54 8189fa99f7c98b1b 6ad806abcf93c8f8 66e1a1918dca9607 c384f3dfc60ec63a 9f42997ad8749585 307349a330fe74f6 d030f6dd4505e09c 13b07777432ecf75 824d951e9769df42 feacabfe2cb0aec6 eb86882e0873801b 123bb8fd87b7d9e2 8539e5a2686c2f91 b0454cdf7b34bdab 16c9ce803f090eb6 --set DATA_CONFIG.DATA_ROOT $TMPDIR MODEL.CONTEXT_ENCODER.mse_coeff 1.0 MODEL.CONTEXT_ENCODER.std_coeff 1.0 MODEL.CONTEXT_ENCODER.cov_coeff 0.0 MODEL.CONTEXT_ENCODER.USE_ATTN_POOL False
+torchrun --nproc_per_node=1 --rdzv_endpoint=localhost:${PORT} train.py --launcher pytorch --cfg_file /home/atuin/v103fe/v103fe12/MTR/tools/cfgs/waymo/mtr+100_percent_data_jepa_with_decoder.yaml --batch_size=20 --epochs=400 --extra_tag=Decoder_wo_t_1_1_0001 --tcp_port=$PORT --workers=8 --not_eval_with_train --max_ckpt_save_num=5 --scenario_id 103718491a34269 3c4c59a7d820467 cd7fdd75bfeddbe7 f13e51dfe26339b3 d2b602df5a0b1a54 8189fa99f7c98b1b 6ad806abcf93c8f8 66e1a1918dca9607 c384f3dfc60ec63a 9f42997ad8749585 307349a330fe74f6 d030f6dd4505e09c 13b07777432ecf75 824d951e9769df42 feacabfe2cb0aec6 eb86882e0873801b 123bb8fd87b7d9e2 8539e5a2686c2f91 b0454cdf7b34bdab 16c9ce803f090eb6 --set DATA_CONFIG.DATA_ROOT $TMPDIR MODEL.CONTEXT_ENCODER.JEPA_WEIGHTS_PATH /home/atuin/v103fe/v103fe12/MTR/output/home/atuin/v103fe/v103fe12/MTR/tools/cfgs/waymo/jepa_loss_trial_no_timestamps_batch_norm/Overfit_wo_t_1_1_0001/ckpt/checkpoint_epoch_400.pth DATA_CONFIG.TIMESTAMP_EMBEDDING False MODEL.CONTEXT_ENCODER.NUM_INPUT_ATTR_AGENT 17
 
 # Deactivate the virtual environment at the end
 deactivate
