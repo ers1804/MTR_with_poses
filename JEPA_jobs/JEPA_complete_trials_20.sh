73783418990f42f760189cbd72f405a2bb1182af
Erik Schuetz <erik.schuetz@hm.edu>
2024-11-12 09:48:47 +0100

    Configs and job descriptions.

--- JEPA_jobs/JEPA_complete_trials_20.sh
+++ JEPA_jobs/JEPA_complete_trials_20.sh
@@ -1,10 +1,10 @@
 #!/bin/bash -l
-#SBATCH --job-name=jepa_decoder
-#SBATCH --output=/home/atuin/v103fe/v103fe12/outputs/jepa_decoder_%j.txt
+#SBATCH --job-name=jepa_loss_trial
+#SBATCH --output=/home/atuin/v103fe/v103fe12/outputs/jepa_complete_%j.txt
 #SBATCH --time=24:00:00
 #SBATCH --nodes=1
-#SBATCH --cpus-per-task=64
-#SBATCH --gres=gpu:a100:8 -C a100_80
+#SBATCH --cpus-per-task=16
+#SBATCH --gres=gpu:a40:2
 #SBATCH --export=NONE
 
 unset SLURM_EXPORT_ENV
@@ -22,9 +22,13 @@ STORAGE_DIR="$(ws_find jepa_data)"
 # the -P parameter defines the number of parallel processes, something like 4-8 should work well
 ls -1 $STORAGE_DIR/archives_val | xargs -P 8 -I{} tar xzf $STORAGE_DIR/archives_val/{} -C $TMPDIR
 ls -1 $STORAGE_DIR/archives_train | xargs -P 8 -I{} tar xzf $STORAGE_DIR/archives_train/{} -C $TMPDIR
-
 cp $WORK/processed_scenarios_training_infos.pkl $TMPDIR/processed_scenarios_training_infos.pkl
 cp $WORK/processed_scenarios_val_infos.pkl $TMPDIR/processed_scenarios_val_infos.pkl
+
+# Unpack training data to $TMPDIR
+#cd $TMPDIR
+#tar xzf $WORK/mtr_training_wo_poses.tar.gz
+
 set -x
 
 while true
@@ -41,7 +45,9 @@ cd /home/atuin/v103fe/v103fe12/MTR/tools
 
 export OMP_NUM_THREADS=64
 
-torchrun --nproc_per_node=8 --rdzv_endpoint=localhost:${PORT} train.py --launcher pytorch --cfg_file /home/atuin/v103fe/v103fe12/MTR/tools/cfgs/waymo/mtr+20_percent_data.yaml --batch_size=160 --epochs=30 --extra_tag=Base_20 --tcp_port=$PORT --workers=8 --not_eval_with_train --max_ckpt_save_num=5 --set DATA_CONFIG.DATA_ROOT $TMPDIR
+
+torchrun --nproc_per_node=2 --rdzv_endpoint=localhost:${PORT} train.py --launcher pytorch --cfg_file /home/atuin/v103fe/v103fe12/MTR/tools/cfgs/waymo/jepa_wt_batch_20.yaml --batch_size=40 --epochs=40 --extra_tag=Trial_1_1_0_Cosine --tcp_port=$PORT --workers=8 --max_ckpt_save_num=5 --ckpt_save_interval=2 --set DATA_CONFIG.DATA_ROOT $TMPDIR
+
 
 # Deactivate the virtual environment at the end
 deactivate
