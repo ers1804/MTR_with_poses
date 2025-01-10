b20cfa8ba48f6a92d556f3ca04bbf08fb6b85ca5
Erik <erik.schuetz@hm.edu>
2025-01-10 08:44:37 +0100

    Fixed batch and layer norm stuff.

--- mtr/models/context_encoder/mtr_encoder.py
+++ mtr/models/context_encoder/mtr_encoder.py
@@ -322,8 +322,10 @@ class JEPATransformerEncoder(nn.Module):
             else:
                 features = center_objects_feature
             if self.lnorm:
-                features = self.batch_norm(torch.nn.functional.layer_norm(features, (features.size(-1),))) if self.use_batch_norm else torch.nn.functional.layer_norm(features, (features.size(-1),))
-            return self.batch_norm(features) if self.use_batch_norm else features
+                features = torch.nn.functional.layer_norm(features, (features.size(-1),))
+            if self.use_batch_norm:
+                features = self.batch_norm(features)
+            return features
 
 class JEPAEncoder(nn.Module):
     def __init__(self, config):
@@ -614,6 +616,54 @@ class JEPAEncoder(nn.Module):
         # Weighted loss
         loss = (mse_coeff * mse_loss + std_coeff * std_loss + cov_coeff * cov_loss)
         return loss, (mse_coeff * mse_loss, std_coeff * std_loss, cov_coeff * cov_loss)
+    
+
+    def get_jepa_loss_with_map(self, output_encoder, output_target_encoder, map_encoder, map_target_encoder, mse_coeff=1.0, std_coeff=1.0, cov_coeff=0.04):
+        num_center_objects, d_model = output_encoder.shape
+        class AllReduce(torch.autograd.Function):
+
+            @staticmethod
+            def forward(ctx, x):
+                if (
+                    dist.is_available()
+                    and dist.is_initialized()
+                    and (dist.get_world_size() > 1)
+                ):
+                    x = x.contiguous() / dist.get_world_size()
+                    dist.all_reduce(x)
+                return x
+
+            @staticmethod
+            def backward(ctx, grads):
+                return grads
+        # MSE loss
+        mse_loss = self.smooth_l1_loss(output_encoder, output_target_encoder)
+        map_mse_loss = self.smooth_l1_loss(map_encoder, map_target_encoder)
+        mse_loss = (mse_loss + map_mse_loss) / 2
+        #mse_loss = AllReduce.apply(mse_loss)
+
+        # Variance loss
+        # Turn encoded features into [num_center_objects, d_model]
+        #output_encoder = output_encoder - torch.mean(output_encoder, dim=0)
+        #output_target_encoder = output_target_encoder - torch.mean(output_target_encoder, dim=0)
+        std_encoder = torch.sqrt(output_encoder.var(dim=0) + 0.0001)
+        #std_target_encoder = torch.sqrt(output_target_encoder.var(dim=0) + 0.0001)
+        std_loss = torch.mean(torch.nn.functional.relu(1 - std_encoder)) / 2 #+ torch.mean(torch.nn.functional.relu(2 - std_target_encoder)) / 2
+        #std_loss = AllReduce.apply(std_loss)
+
+        # Covariance loss
+        def off_diagonal(x):
+            n, m = x.shape
+            assert n == m
+            return x.flatten()[:-1].view(n - 1, n + 1)[:, 1:].flatten()
+        cov_encoder = (output_encoder.T @ output_encoder) / (num_center_objects - 1)
+        #cov_target_encoder = (output_target_encoder.T @ output_target_encoder) / (num_center_objects - 1)
+        cov_loss = off_diagonal(cov_encoder).pow_(2).sum().div(d_model) #+ off_diagonal(cov_target_encoder).pow_(2).sum().div(d_model)
+        #cov_loss = AllReduce.apply(cov_loss)
+
+        # Weighted loss
+        loss = (mse_coeff * mse_loss + std_coeff * std_loss + cov_coeff * cov_loss)
+        return loss, (mse_coeff * mse_loss, std_coeff * std_loss, cov_coeff * cov_loss)
 
 
     def forward(self, batch_dict, target=False):
@@ -749,8 +799,10 @@ class JEPAEncoder(nn.Module):
             else:
                 features = center_objects_feature
             if self.lnorm:
-                features = self.batch_norm(torch.nn.functional.layer_norm(features, (features.size(-1),))) if self.use_batch_norm else torch.nn.functional.layer_norm(features, (features.size(-1),))
-            return self.batch_norm(features) if self.use_batch_norm else features
+                features = torch.nn.functional.layer_norm(features, (features.size(-1),))
+            if self.use_batch_norm:
+                features = self.batch_norm(features)
+            return features
 
 
 
